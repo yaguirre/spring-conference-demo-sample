501ceb6a0483a01e05783b7aeba32faf89aac2d9
Yorman Aguirre <yaguirre@eafit.edu.co>
2020-08-07 01:10:24 -0500

    Fixed ConfigurationPersistance comment

--- src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -8,13 +8,13 @@ import javax.sql.DataSource;
 
 @Configuration
 public class PersistenceConfiguration {
-    @Bean
-    public DataSource dataSource(){
-        /*DataSourceBuilder builder = DataSourceBuilder.create();
-        builder.url("jdbc:postgresql://localhost:5432/conference_app");
-        builder.username("postgres");
-        builder.password("Welcome");
-        System.out.print("My custom Datasource bean has been initialized and set");
-        return builder.build();*/
-    }
+//    @Bean
+//    public DataSource dataSource(){
+//        DataSourceBuilder builder = DataSourceBuilder.create();
+//        builder.url("jdbc:postgresql://localhost:5432/conference_app");
+//        builder.username("postgres");
+//        builder.password("Welcome");
+//        System.out.print("My custom Datasource bean has been initialized and set");
+//        return builder.build();
+//    }
 }
