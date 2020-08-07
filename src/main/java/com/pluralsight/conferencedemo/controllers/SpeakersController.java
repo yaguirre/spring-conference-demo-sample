4f6636b6f564c582e784a16db10751ba0d3e4f09
Yorman Aguirre <yaguirre@eafit.edu.co>
2020-08-07 00:38:09 -0500

    Adding sessions and speakers controllers, models and repositories

---
+++ src/main/java/com/pluralsight/conferencedemo/controllers/SpeakersController.java
@@ -0,0 +1,33 @@
+package com.pluralsight.conferencedemo.controllers;
+
+import com.pluralsight.conferencedemo.models.Speaker;
+import com.pluralsight.conferencedemo.repositories.SpeakerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/speakers")
+public class SpeakersController {
+    @Autowired
+    private SpeakerRepository speakerRepository;
+
+    @GetMapping
+    public List<Speaker> list(){
+        return speakerRepository.findAll();
+    }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public Speaker get(@PathVariable Long id){
+        return speakerRepository.getOne(id);
+    }
+
+    @PostMapping
+    public Speaker create(@RequestBody final Speaker speaker){
+        return speakerRepository.saveAndFlush(speaker);
+    }
+
+
+}
